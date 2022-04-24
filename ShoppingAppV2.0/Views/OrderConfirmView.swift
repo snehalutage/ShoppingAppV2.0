2788178c2ed15192e46951ce24236aa4d04c8b11
Snehal Utage <snehal19su@gmail.com>
2022-04-24 13:38:41 -0500

    Updated code

---
+++ ShoppingAppV2.0/Views/OrderConfirmView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderConfirmView.swift
+//  ShoppingAppV2.0
+//
+//  Created by Snehal on 4/17/22.
+//
+
+import SwiftUI
+
+struct OrderConfirmView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderConfirmView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderConfirmView()
+    }
+}
